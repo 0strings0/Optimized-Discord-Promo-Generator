11c4fd50d880f57642dec7bf1b6d8e41a26e998e
0strings0 <134749985+0strings0@users.noreply.github.com>
2023-12-23 20:59:16 -0500

    Optimized Further (650/m)

--- gen.py
+++ gen.py
@@ -17,7 +17,8 @@ class PromoGenerator:
     green = '\x1b[32m(+)\x1b[0m'
     yellow = '\x1b[33m(!)\x1b[0m'
 
-    def __init__(self, proxy=None):
+    def __init__(self, session=None, proxy=None):
+        self.session = session or requests.Session()
         self.proxy = proxy
 
     def generate_promo(self):
@@ -36,15 +37,14 @@ class PromoGenerator:
 
         for _ in range(retry_attempts):
             try:
-                with requests.Session() as session:
-                    if self.proxy:
-                        credentials, host = self.proxy.split('@')
-                        user, password = credentials.split(':')
-                        host, port = host.split(':')
-                        formatted_proxy = f"http://{user}:{password}@{host}:{port}"
-                        response = session.post(url, json=data, headers=headers, proxies={'http': formatted_proxy, 'https': formatted_proxy}, timeout=5)
-                    else:
-                        response = session.post(url, json=data, headers=headers, timeout=5)
+                if self.proxy:
+                    credentials, host = self.proxy.split('@')
+                    user, password = credentials.split(':')
+                    host, port = host.split(':')
+                    formatted_proxy = f"http://{user}:{password}@{host}:{port}"
+                    response = self.session.post(url, json=data, headers=headers, proxies={'http': formatted_proxy, 'https': formatted_proxy}, timeout=5)
+                else:
+                    response = self.session.post(url, json=data, headers=headers, timeout=5)
 
                 if response.status_code == 200:
                     token = response.json().get('token')
@@ -58,7 +58,7 @@ class PromoGenerator:
                         return link
                 elif response.status_code == 429:
                     return "rate-limited"
-                elif response.status_code == 504:  
+                elif response.status_code == 504:
                     continue
                 else:
                     continue
@@ -66,7 +66,7 @@ class PromoGenerator:
                 # Hide the specific timeout error
                 continue
             except requests.exceptions.RequestException as e:
-                time.sleep(0.5)  
+                time.sleep(0.5)
 
         return "Max Retries"
 
@@ -85,25 +85,39 @@ class PromoManager:
 
     def start_promo_generation(self):
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_threads) as executor:
-            futures = {executor.submit(self.generate_promo): i for i in range(self.num_threads)}
+            futures = {executor.submit(self.generate_promo, choice(self.proxies) if self.proxies else None): i for i in range(self.num_threads)}
             try:
+                chunk_size = 10  # Adjust chunk size as needed
+                results_buffer = []
+
                 for future in concurrent.futures.as_completed(futures):
                     result = future.result()
-                    if "rate-limited" in result:
-                        print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.yellow} You are being rate-limited!")
-                        quit()
-                    elif "Request failed" in result:
-                        print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.red} {result}")
-                    elif "Max Retries" not in result:  
-                        print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.green} Generated Promo Link : {result}")
-                        with open("promos.txt", "a") as f:
-                            f.write(f"{result}\n")
+                    results_buffer.append(result)
+
+                    if len(results_buffer) >= chunk_size:
+                        self.process_results(results_buffer)
+                        results_buffer = []
+
+                # Process any remaining results
+                if results_buffer:
+                    self.process_results(results_buffer)
             except KeyboardInterrupt:
                 pass
 
-    def generate_promo(self):
-        proxy = choice(self.proxies) if self.proxies else None
-        generator = PromoGenerator(proxy)
+    def process_results(self, results):
+        for result in results:
+            if "rate-limited" in result:
+                print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.yellow} You are being rate-limited!")
+                quit()
+            elif "Request failed" in result:
+                print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.red} {result}")
+            elif "Max Retries" not in result:
+                print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.green} Generated Promo Link : {result}")
+                with open("promos.txt", "a") as f:
+                    f.write(f"{result}\n")
+
+    def generate_promo(self, proxy):
+        generator = PromoGenerator(proxy=proxy)
         while True:
             result = generator.generate_promo()
             if "rate-limited" in result:
@@ -111,7 +125,7 @@ class PromoManager:
                 quit()
             elif "Request failed" in result:
                 print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.red} {result}")
-            elif "Max Retries" not in result:  # Add this condition
+            elif "Max Retries" not in result:
                 print(f"{PromoGenerator.get_timestamp()} {PromoGenerator.green} Generated Promo Link : {result}")
                 with open("promos.txt", "a") as f:
                     f.write(f"{result}\n")
@@ -123,7 +137,6 @@ class PromoManager:
                 ctypes.windll.kernel32.SetConsoleIcon(ctypes.windll.shell32.ExtractIconW(0, icon_path, 0))
             except Exception as e:
                 print(f"Failed to set the window icon: {e}")
-
 
 if __name__ == "__main__":
     manager = PromoManager()
